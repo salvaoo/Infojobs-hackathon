33b51883e092eb5824148c369eb70cf3d183d263
salvaoo <salvadorgilrosales@outlook.es>
2023-05-17 00:16:35 +0100

    Init git

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+
+export default function IndexPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+          {siteConfig.description}
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
+          Accessible and customizable components that you can copy and paste
+          into your apps. Free. Open Source. And Next.js 13 Ready.
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <Link
+          href={siteConfig.links.docs}
+          target="_blank"
+          rel="noreferrer"
+          className={buttonVariants({ size: "lg" })}
+        >
+          Documentation
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.shadcnGithub}
+          className={buttonVariants({ variant: "outline", size: "lg" })}
+        >
+          GitHub
+        </Link>
+      </div>
+    </section>
+  )
+}
---
+++ src/config/site.ts
@@ -0,0 +1,20 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+   name: "Next.js",
+   description:
+     "A Next.js template with shadcn/ui components. This template is a TypeScript version.",
+   mainNav: [
+     {
+       title: "Home",
+       href: "/",
+     },
+   ],
+   links: {
+     twitter: "https://twitter.com/salvagr_",
+     shadcnGithub: "https://github.com/shadcn/ui",
+     github: "https://github.com/salvaoo",
+     docs: "https://ui.shadcn.com",
+   },
+}
+ 
