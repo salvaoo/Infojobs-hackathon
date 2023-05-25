93e42dae0c04cc5b4e3c4e3ae141ed30173659bb
salvaoo <salvadorgilrosales@outlook.es>
2023-05-25 01:19:34 +0100

    fix(session): Fix login button in the header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -68,7 +68,7 @@ export function SiteHeader() {
                 <span className="sr-only">Twitter</span>
               </div>
             </Link> */}
-            {session.access_token.length === 0 ? (
+            {session.expires_in === 0 ? (
               <Link href="https://www.infojobs.net/api/oauth/user-authorize/index.xhtml?scope=CV,CANDIDATE_PROFILE_WITH_EMAIL,CANDIDATE_READ_CURRICULUM_CVTEXT,CANDIDATE_READ_CURRICULUM_EDUCATION,CANDIDATE_READ_CURRICULUM_EXPERIENCE,CANDIDATE_READ_CURRICULUM_FUTURE_JOB,CANDIDATE_READ_CURRICULUM_PERSONAL_DATA,CANDIDATE_READ_CURRICULUM_SKILLS&client_id=912f8125fe094a12a417eabbb3137321&redirect_uri=https://infojobs-hackathon-ebon.vercel.app/api/auth/callback/infojobs&response_type=code">
                 <Button size="sm">
                   Login
