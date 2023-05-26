d40ce4411f1d7a2a66f1187683ade926305fd191
salvaoo <salvadorgilrosales@outlook.es>
2023-05-26 01:37:48 +0100

    Fix cv route

--- src/app/api/curriculum/[[...slug]]/route.ts
+++ src/app/api/curriculum/[[...slug]]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server"
 import { cookies } from 'next/headers'
+import { SessionType } from "@/atoms/session";
 
 export async function GET(
    request: Request,
@@ -10,38 +11,38 @@ export async function GET(
    },
 ) {
    const cookieStore = cookies()
-   const session = cookieStore.get('session')
 
-   if (!session) {
+   if (cookieStore.has('session')) {
       return NextResponse.json({ error: 'session error' }, { status: 401 })
    }
+
+   const session = JSON.parse(cookieStore.get('session')?.value as string) as SessionType
 
    const slug = params.slug;
 
    return NextResponse.json({ slug, session })
 
-   // const CLIENT = process.env.IJ_CLIENT_ID;
-   // const SECRET = process.env.IJ_CLIENT_SECRET;
+   const CLIENT = process.env.IJ_CLIENT_ID;
+   const SECRET = process.env.IJ_CLIENT_SECRET;
 
-   // const authToken = btoa(`${CLIENT}:${SECRET}`);
+   const authToken = btoa(`${CLIENT}:${SECRET}`);
 
-   // // if slug have a 2 length, return the curriculum list
-   // if (slug?.length === 2) {
-   //    const curriculumId = slug[0]
-   //    const action = slug[1]
+   if (slug?.length === 2) {
+      const curriculumId = slug[0]
+      const action = slug[1]
 
-   //    return NextResponse.json({ curriculumId, action })
-   // }
+      return NextResponse.json({ curriculumId, action })
+   }
 
-   // const res = await fetch(`${process.env.IJ_API_URL}/2/curriculum`, {
-   //    headers: {
-   //       'Content-Type': 'application/json',
-   //       'Authorization': `Basic ${authToken}, Bearer ${process.env.IJ_ACCESS_TOKEN}`
-   //    }
-   // })
-   // const curriculum = await res.json()
+   const res = await fetch(`${process.env.IJ_API_URL}/2/curriculum`, {
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': `Basic ${authToken}, Bearer ${session.access_token}`
+      }
+   })
+   const curriculum = await res.json()
 
-   // return NextResponse.json({ curriculum })
+   return NextResponse.json({ curriculum })
 
 
    // if (id) {
