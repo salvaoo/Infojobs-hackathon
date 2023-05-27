adecb9b7e6280a69f40e0260dd059ce18403a4b6
salvaoo <salvadorgilrosales@outlook.es>
2023-05-27 02:20:42 +0100

    feat(session): Fix the profile error | suspense

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -11,18 +11,18 @@ import { authOptions } from "@/lib/auth"
 // This function fetches the profile data from the server
 const getProfile = async () => {
    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/curriculum`)
-   const { curriculum, cvText, education, experience, futurejob, personaldata, skill } = await res.json() as ProfileProps
+   const profile = await res.json() as ProfileProps
 
    // Returns an object with all the profile data
-   return { curriculum, cvText, education, experience, futurejob, personaldata, skill }
+   return profile
 }
 
 export const Profile = async () => {
-   const session = await getServerSession(authOptions)
-   const { curriculum, cvText, education, experience, futurejob, personaldata, skill } = await getProfile()
+   // const session = await getServerSession(authOptions)
+   const profile = await getProfile()
    
    // If session is undefined, show the login button
-   if (curriculum.id === 0) {
+   if (profile?.curriculum.id === 0) {
       return (
          <Alert>
             <AlertCircle className="h-4 w-4" />
@@ -39,12 +39,12 @@ export const Profile = async () => {
       <section className={`${fontSans.className}`}>
          <div className="flex flex-col gap-3 justify-center items-center">
             <Avatar className="w-20 h-20">
-               <AvatarImage src={`https://www.infojobs.net/candidato.foto?id_candidato=${curriculum.code}&CameFrom=perfil`} />
+               <AvatarImage src={`https://www.infojobs.net/candidato.foto?id_candidato=${profile?.curriculum.code}&CameFrom=perfil`} />
                <AvatarFallback>IJ</AvatarFallback>
             </Avatar>
             <div className="space-y-2">
-               <h2 className="text-lg font-semibold leading-none tracking-tight">{curriculum.name}</h2>
-               <p className="text-sm text-muted-foreground">{experience[0].onCourse && experience[0].job}</p>
+               <h2 className="text-lg font-semibold leading-none tracking-tight">{profile?.curriculum?.name}</h2>
+               <p className="text-sm text-muted-foreground">{profile?.experience[0]?.onCourse && profile?.experience[0]?.job}</p>
             </div>
          </div>
       </section>
