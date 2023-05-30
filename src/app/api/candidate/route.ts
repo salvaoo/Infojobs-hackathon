5d2ef991be7d4a846156c56d7d48f841d212dbee
salvaoo <salvadorgilrosales@outlook.es>
2023-05-30 12:28:22 +0100

    fix: Add the "force-dynamic" to api

--- src/app/api/candidate/route.ts
+++ src/app/api/candidate/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server"
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request) {
    const session = await getServerSession(authOptions)
 
